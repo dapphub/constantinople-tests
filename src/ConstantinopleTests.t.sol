5f7002c8236a4f912763b998aa8f29825a24a7f1
Lev Livnev <lev@liv.nev.org.uk>
2019-02-13 14:12:07 +0000

    correct some silly mistakes, now it compiles

--- src/ConstantinopleTests.t.sol
+++ src/ConstantinopleTests.t.sol
@@ -4,8 +4,8 @@ import "ds-test/test.sol";
 
 import "./ConstantinopleTests.sol";
 
-contract DeadCode {
-    function dummy() external returns (bytes32);
+contract DeadCode{
+    function dummy() external returns (uint256);
 }
 
 contract ConstantinopleTestsTest is DSTest {
@@ -14,11 +14,11 @@ contract ConstantinopleTestsTest is DSTest {
     // this 13 byte-long initcode simply returns 0xdeadbeef:
     // PUSH4  de     ad     be     ef     PUSH1  00     MSTORE PUSH1  04     PUSH1  00     RETURN
     // 63     de     ad     be     ef     60     00     52     60     04     60     00     f3
-    bytes32 deadcode        = 0x63deadbeef60005260046000f300000000000000000000000000000000000000
+    bytes32 deadcode        = 0x63deadbeef60005260046000f300000000000000000000000000000000000000;
     // this 25 byte-long initcode returns deadcode (but without the padding)
     // PUSH1  0d     PUSH1  0c     PUSH1  00     CODECO PUSH1  0d     PUSH1  00     RETURN deadcode
     // 60     0d     60     0c     60     00     39     60     0d     60     00     f3
-    bytes32 deploysdeadcode = 0x600d600c600039600d6000f363deadbeef60005260046000f300000000000000
+    bytes32 deploysdeadcode = 0x600d600c600039600d6000f363deadbeef60005260046000f300000000000000;
 
     function setUp() public {
         tests = new ConstantinopleTests();
@@ -48,36 +48,43 @@ contract ConstantinopleTestsTest is DSTest {
         uint256 h;
 
         assembly {
-          let pos := mload(0x40)
-          mstore(pos, deadcode)
-          a := create(0, pos, 13)
+          let top := mload(0x40)
+          mstore(top, sload(deadcode_slot))
+          a := create(0, top, 13)
           h := extcodehash(a)
         }
-        assertEq(h, keccak256(0xdeadbeef));
+
+        uint256 expected_h;
+        assembly {
+            let top := mload(0x40)
+            mstore(top, 0xdeadbeef)
+            expected_h := keccak256(top, 4)
+        }
+        assertEq(h, expected_h);
     }
 
     // address of account created by CREATE2 is
     // keccak256(0xff + address + salt + keccak256(init_code))[12:]
     function test_create2_1() public {
         address a;
-        bytes32 salt = 0xfacefeed
+        uint256 salt = 0xfacefeed;
         assembly {
-          let pos := mload(0x40)
-          mstore(pos, deadcode)
-          a := create2(0, pos, 13, salt)
+          let top := mload(0x40)
+          mstore(top, sload(deadcode_slot))
+          a := create2(0, top, 13, salt)
         }
 
         address expected_a;
 
         assembly {
-          let pos := mload(0x40)
-          mstore(pos, deadcode)
-          let inithash := keccak256(pos, 13)
-          mstore(pos, 0xff)
-          mstore(add(pos, 1), address)
-          mstore(add(pos, 21), salt)
-          mstore(add(pos, 53), inithash)
-          expected_a := xor(keccak256(pos, 85), 0x0000000000000000000000001111111111111111111111111111111111111111)
+          let top := mload(0x40)
+          mstore(top, sload(deadcode_slot))
+          let inithash := keccak256(top, 13)
+          mstore(top, 0xff)
+          mstore(add(top, 1), address)
+          mstore(add(top, 21), salt)
+          mstore(add(top, 53), inithash)
+          expected_a := xor(keccak256(top, 85), 0x0000000000000000000000001111111111111111111111111111111111111111)
         }
 
         assertEq(a, expected_a);
@@ -85,11 +92,11 @@ contract ConstantinopleTestsTest is DSTest {
     // calling a CREATE2 contract works as expected
     function test_create2_2() public {
         address a;
-        bytes32 salt = 0xfacefeed
+        uint256 salt = 0xfacefeed;
         assembly {
-          let pos := mload(0x40)
-          mstore(pos, deploysdeadcode)
-          a := create2(0, pos, 25, salt)
+          let top := mload(0x40)
+          mstore(top, sload(deploysdeadcode_slot))
+          a := create2(0, top, 25, salt)
         }
 
         assertEq(DeadCode(a).dummy(), 0xdeadbeef);
